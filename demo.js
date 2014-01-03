cb248fa61bb67185463e330420648b2af54aefa3
deathcap <deathcap@gmx.co.uk>
2014-01-02 17:43:51 -0800

    Fix alpha, set to be 255

--- demo.js
+++ demo.js
@@ -8,14 +8,16 @@ var height = 3;
 var canvas = document.createElement('canvas');
 canvas.setAttribute('style', 'border: 1px solid black; width: 100px; height: 100px;');
 var context = canvas.getContext('2d');
-var imageData = context.getImageData(0, 0, width, height);
+var imageData = context.createImageData(width, height);
 console.log(imageData);
 imageData.data.set([
-    100,100,100,100, 100,100,100,100, 100,100,100,100,
-    255,255,255,255, 100,100,100,100, 255,255,255,255,
-    100,100,100,100, 100,100,100,100, 100,100,100,100
+    100,100,100,255, 100,100,100,255, 100,100,100,255,
+    255,255,255,255, 100,100,100,255, 255,255,255,255,
+    100,100,100,255, 100,100,100,255, 100,100,100,255
     ]);
-console.log(imageData);
+console.log(imageData.data);
+console.log(imageData.width);
+console.log(imageData.height);
 context.putImageData(imageData, 0, 0);
 
 document.body.appendChild(canvas);

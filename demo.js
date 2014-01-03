95ab9e4b4057577eb845c8fe95389ef9e482e33d
deathcap <deathcap@gmx.co.uk>
2014-01-02 17:39:10 -0800

    Try .data.set on context.getImageData instead. Fixes type error

--- demo.js
+++ demo.js
@@ -4,19 +4,18 @@ var noisecomp = require('./');
 
 var width = 3;
 var height = 3;
-var arrayBuffer = new ArrayBuffer(width * height * 4);
-var data = new Uint8ClampedArray(arrayBuffer);
-data.set([
-    10,10,10,10, 10,10,10,10, 10,10,10,10,
-    80,80,80,80, 10,10,10,10, 80,80,80,80,
-    10,10,10,10, 10,10,10,10, 10,10,10,10
-    ]);
-var imageData = new ImageData(data, width, height);
 
 var canvas = document.createElement('canvas');
-canvas.setAttribute('style', 'border: 1px solid black;');
+canvas.setAttribute('style', 'border: 1px solid black; width: 100px; height: 100px;');
 var context = canvas.getContext('2d');
-//var imageData = context.createImageData(3, 3);
+var imageData = context.getImageData(0, 0, width, height);
+console.log(imageData);
+imageData.data.set([
+    100,100,100,100, 100,100,100,100, 100,100,100,100,
+    255,255,255,255, 100,100,100,100, 255,255,255,255,
+    100,100,100,100, 100,100,100,100, 100,100,100,100
+    ]);
+console.log(imageData);
 context.putImageData(imageData, 0, 0);
 
 document.body.appendChild(canvas);

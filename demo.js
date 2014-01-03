6dff0aed3f7a5a7422bf0cdc00185afff84c3956
deathcap <deathcap@gmx.co.uk>
2014-01-02 19:36:39 -0800

    Show step + gradient; finally got to 2nd image of http://accidentalnoise.sourceforge.net/minecraftworlds.html

--- demo.js
+++ demo.js
@@ -2,14 +2,14 @@
 
 var noisecomp = require('./');
 
-var gradient = noisecomp.generators.gradientY({minY:0, maxY:100});
+var gradient = noisecomp.generators.gradientY({minY:0, maxY:1000});
 var source = noisecomp.generators.noise({seed: 43});
 var scale = noisecomp.transformers.scale(0,1, 0,0.75);
 var step = noisecomp.transformers.step(0.5, 0, 1);
 
 var f = function(x, y) {
-  return step(scale(source(x, y)));
-  return gradient(x, y);
+  //return step(scale(source(x, y)));
+  return step(gradient(x, y));
 };
 
 noisecomp.test(f);

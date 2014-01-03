91d26afa30f76c36b1cb16ceeaedee818a3de8ad
deathcap <deathcap@gmx.co.uk>
2014-01-02 18:21:37 -0800

    Move showCanvas

--- demo.js
+++ demo.js
@@ -4,35 +4,6 @@ var noisecomp = require('./');
 var ndarray = require('ndarray');
 var alea = require('alea');
 
-var showCanvas = function(indexData, width, height, colors) {
-  if (colors === undefined) 
-    colors = [
-    [255,255,255,255],   // 0 white
-    [0,0,0,255]          // 1 black
-  ];
-
-  var canvases = document.getElementsByTagName('canvas');
-  var canvas = (canvases.length > 0) ? canvases[0] : document.createElement('canvas');
-
-  canvas.setAttribute('style', 'border: 1px solid black; width: '+width+'px; height: '+height+'px;');
-  var context = canvas.getContext('2d');
-  var imageData = context.createImageData(width, height);
-  var rgbaData = imageData.data;
-
-  var i = indexData.length;
-  while(i--) {
-    var index = indexData[i];
-    rgbaData[i * 4 + 0] = colors[index][0]; // TODO: use ndarray?
-    rgbaData[i * 4 + 1] = colors[index][1];
-    rgbaData[i * 4 + 2] = colors[index][2];
-    rgbaData[i * 4 + 3] = colors[index][3];
-  }
-
-  context.putImageData(imageData, 0, 0);
-
-  document.body.appendChild(canvas);
-}
-
 var width = 1000;
 var height = 1000;
 
@@ -47,8 +18,6 @@ while (n-- > 0) {
   if (y > 100) data[n] = 0;
 }
 
-window.showCanvas = showCanvas;
-
-showCanvas(data, width, height);
+noisecomp.showCanvas(data, width, height);
 //window.showCanvas = showCanvas;
 
--- index.js
+++ index.js
@@ -1,3 +1,34 @@
+'use strict';
+
+module.exports.showCanvas = function(indexData, width, height, colors) {
+  if (colors === undefined) 
+    colors = [
+    [255,255,255,255],   // 0 white
+    [0,0,0,255]          // 1 black
+  ];
+
+  var canvases = document.getElementsByTagName('canvas');
+  var canvas = (canvases.length > 0) ? canvases[0] : document.createElement('canvas');
+
+  canvas.setAttribute('style', 'border: 1px solid black; width: '+width+'px; height: '+height+'px;');
+  var context = canvas.getContext('2d');
+  var imageData = context.createImageData(width, height);
+  var rgbaData = imageData.data;
+
+  var i = indexData.length;
+  while(i--) {
+    var index = indexData[i];
+    rgbaData[i * 4 + 0] = colors[index][0]; // TODO: use ndarray?
+    rgbaData[i * 4 + 1] = colors[index][1];
+    rgbaData[i * 4 + 2] = colors[index][2];
+    rgbaData[i * 4 + 3] = colors[index][3];
+  }
+
+  context.putImageData(imageData, 0, 0);
+
+  document.body.appendChild(canvas);
+}
+
 
 module.exports.gradient = function(opts) {
   return function(x, y) {
@@ -6,4 +37,6 @@ module.exports.gradient = function(opts) {
     return dx + dy;
   };
 };
+
+window.noisecomp = module.exports; // for debugging
 

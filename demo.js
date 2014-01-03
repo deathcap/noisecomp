c603c3e5b3c9e7c2e6008d9917846931fcf9c3a4
deathcap <deathcap@gmx.co.uk>
2014-01-02 17:58:35 -0800

    Convert color indexed data to RGBA; show stripes

--- demo.js
+++ demo.js
@@ -1,23 +1,39 @@
 'use strict';
 
 var noisecomp = require('./');
+var ndarray = require('ndarray');
 
-var width = 3;
-var height = 3;
+var showCanvas = function(indexData, width, height, colors) {
+  var canvas = document.createElement('canvas');
+  canvas.setAttribute('style', 'border: 1px solid black; width: '+width+'px; height: '+height+'px;');
+  var context = canvas.getContext('2d');
+  var imageData = context.createImageData(width, height);
+  var rgbaData = imageData.data;
 
-var canvas = document.createElement('canvas');
-canvas.setAttribute('style', 'border: 1px solid black; width: 100px; height: 100px;');
-var context = canvas.getContext('2d');
-var imageData = context.createImageData(width, height);
-console.log(imageData);
-imageData.data.set([
-    100,100,100,255, 100,100,100,255, 100,100,100,255,
-    255,255,255,255, 100,100,100,255, 255,255,255,255,
-    100,100,100,255, 100,100,100,255, 100,100,100,255
-    ]);
-console.log(imageData.data);
-console.log(imageData.width);
-console.log(imageData.height);
-context.putImageData(imageData, 0, 0);
+  var i = indexData.length;
+  while(i--) {
+    var index = indexData[i];
+    rgbaData[i * 4 + 0] = colors[index][0]; // TODO: use ndarray?
+    rgbaData[i * 4 + 1] = colors[index][1];
+    rgbaData[i * 4 + 2] = colors[index][2];
+    rgbaData[i * 4 + 3] = colors[index][3];
+  }
 
-document.body.appendChild(canvas);
+  context.putImageData(imageData, 0, 0);
+
+  document.body.appendChild(canvas);
+}
+
+var width = 1000;
+var height = 1000;
+
+var data = new Uint8Array(width * height);
+var n = data.length;
+while (n-- > 0)
+  data[n] = n % 2;
+
+var colors = [
+  [255,255,255,255],   // 0 white
+  [0,0,0,255]          // 1 black
+];
+showCanvas(data, width, height, colors);
